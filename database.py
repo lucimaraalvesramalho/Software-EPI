29efc315533ce73e946767957b51c8355888ed22
BlackGhostt6 <gabrielsam.276@gmail.com>
2026-06-04 00:37:01 -0300

    Atualização de segurança

--- database.py
+++ database.py
@@ -1,11 +1,16 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+# carregar variáveis de ambiente
+load_dotenv()
 
 # configurações de conexão com o banco de dados
 db_config = {
-    'host': 'localhost',
-    'database': 'controle_epi',
-    'user': 'root',
-    'password': '1513'
+    'host': os.getenv('DB_HOST'),
+    'database': os.getenv('DB_NAME'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD')
 }
 
 def get_db_connection():
